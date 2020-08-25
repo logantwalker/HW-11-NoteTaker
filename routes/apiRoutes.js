ae20e3baf7885ed621404a410b9d0e34233e2460
L.Walker <logantwalker95@gmail.com>
2020-08-25 17:26:43 -0400

    all requests functioning

---
+++ routes/apiRoutes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+var db = [];
+
+module.exports = function(app){
+    app.get("/api/notes",function(req,res){
+        db = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        if(db.length){
+            let id=1;
+            for(i of db){
+                i.id = id;
+                id++;
+            }
+        }
+        
+        return res.json(db);
+    });
+    
+    app.post("/api/notes", function(req, res) {
+        db.push(req.body);
+        
+        let id=1;
+        for(i of db){
+            i.id = id;
+            id++;
+        }
+        fs.writeFileSync('./db/db.json',JSON.stringify(db),'utf8');
+        res.json(db);
+    });
+
+    app.delete("/api/notes/:id", function(req,res){
+        let queryID = req.params.id;
+        db = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        for(i in db){
+            if(queryID == db[i].id){
+                db.splice(i,1);
+                fs.writeFileSync('./db/db.json',JSON.stringify(db),'utf8');
+            }
+        }
+        res.send();
+    });
+}
