9b406e032f2e3d90b0dc76057f94a6b0d4c9773c
L.Walker <logantwalker95@gmail.com>
2020-08-25 17:43:25 -0400

    added dynamic PORT

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+var PORT = process.env.PORT || 3001;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
